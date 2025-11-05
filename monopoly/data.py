57c1095b2b477b43c1162eb2c5c59d04cd6fc102
yossi lerer <ylerer770@gmail.com>
2025-11-05 15:28:13 +0200

    start project

---
+++ monopoly/data.py
@@ -0,0 +1,69 @@
+
+
+tiles_data = [
+    {"name": "Start", "type": "start", "city": None},
+
+    # Tel Aviv - Most Expensive
+    {"name": "Allenby", "type": "property", "price": 400, "rent": 150, "city": "Tel Aviv"},
+    {"name": "Dizengoff", "type": "property", "price": 420, "rent": 160, "city": "Tel Aviv"},
+    {"name": "Ben Yehuda", "type": "property", "price": 440, "rent": 170, "city": "Tel Aviv"},
+
+    {"name": "Income Tax", "type": "tax", "amount": 50, "city": None},
+    {"name": "North Train Station", "type": "property", "price": 200, "rent": 50, "city": None},
+
+    # Jerusalem
+    {"name": "Knesset", "type": "property", "price": 360, "rent": 140, "city": "Jerusalem"},
+    {"name": "Jaffa Street", "type": "property", "price": 380, "rent": 150, "city": "Jerusalem"},
+    {"name": "Malha Mall", "type": "property", "price": 400, "rent": 160, "city": "Jerusalem"},
+
+    {"name": "Chance", "type": "bonus", "amount": 60, "city": None},
+    {"name": "Jail", "type": "jail", "city": None},
+
+    # Ramat Gan
+    {"name": "Bursa", "type": "property", "price": 300, "rent": 120, "city": "Ramat Gan"},
+    {"name": "Jabotinsky", "type": "property", "price": 320, "rent": 130, "city": "Ramat Gan"},
+    {"name": "Moshe Aviv Towers", "type": "property", "price": 340, "rent": 140, "city": "Ramat Gan"},
+
+    {"name": "Tax", "type": "tax", "amount": 40, "city": None},
+    {"name": "South Train Station", "type": "property", "price": 200, "rent": 50, "city": None},
+
+    # Haifa
+    {"name": "Sderot Moriah", "type": "property", "price": 260, "rent": 100, "city": "Haifa"},
+    {"name": "Technion", "type": "property", "price": 280, "rent": 110, "city": "Haifa"},
+    {"name": "Hadera", "type": "property", "price": 300, "rent": 120, "city": "Haifa"},
+
+    {"name": "Bonus", "type": "bonus", "amount": 50, "city": None},
+    {"name": "Central Train Station", "type": "property", "price": 200, "rent": 50, "city": None},
+
+    # Eilat
+    {"name": "The Boardwalk", "type": "property", "price": 180, "rent": 60, "city": "Eilat"},
+    {"name": "Red Sea", "type": "property", "price": 200, "rent": 70, "city": "Eilat"},
+    {"name": "Airport", "type": "property", "price": 220, "rent": 80, "city": "Eilat"},
+
+    {"name": "Go To Jail", "type": "go_to_jail", "city": None},
+    {"name": "Luxury Tax", "type": "tax", "amount": 75, "city": None},
+    {"name": "Bonus", "type": "bonus", "amount": 60, "city": None},
+
+    # Haifa 2 (For the last lap to complete 40)
+    {"name": "Haifa Port", "type": "property", "price": 260, "rent": 100, "city": "Haifa"},
+    {"name": "Carmelit", "type": "property", "price": 280, "rent": 110, "city": "Haifa"},
+    {"name": "Dado Beach", "type": "property", "price": 300, "rent": 120, "city": "Haifa"},
+
+    {"name": "East Train Station", "type": "property", "price": 200, "rent": 50, "city": None},
+    {"name": "Bonus", "type": "bonus", "amount": 50, "city": None},
+    {"name": "End", "type": "end", "city": None},
+
+    # tzfat
+    {"name": "Rakefet Hotel", "type": "property", "price": 360, "rent": 140, "city": "Tzfat"},
+    {"name": "Central station", "type": "property", "price": 380, "rent": 150, "city": "Tzfat"},
+    {"name": "Mikve Hari", "type": "property", "price": 400, "rent": 160, "city": "Tzfat"},
+    {"name": "Pizza Yerushlim", "type": "property", "price": 400, "rent": 160, "city": "Tzfat"},
+
+
+    #Tveria
+    {"name": "Kineret hotel", "type": "property", "price": 360, "rent": 140, "city": "Tveria"},
+    {"name": "Kever Hrambm", "type": "property", "price": 380, "rent": 150, "city": "Tveria"},
+    {"name": "Rabbi meir bel h'nes", "type": "property", "price": 400, "rent": 160, "city": "Tveria"},
+    {"name": "Lido", "type": "property", "price": 400, "rent": 160, "city": "Tveria"},
+]
+
